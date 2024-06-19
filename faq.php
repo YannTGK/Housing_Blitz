4c58a6a9bd57a8f34093f59f32605ef689059dcc
Charlotte <r0952503@student.thomasmore.be>
2024-06-19 16:32:31 +0200

    faq picture's

--- faq.php
+++ faq.php
@@ -51,37 +51,41 @@ if(isset($_GET['logout'])) {
         <div class="faq-item">
             <div class="faq-question">
                 <h3>1. Wat is het verschil tussen een huurpremie en een subsidie?</h3>
+                <img src="images/1.png" alt="Het Juridisch Loket Logo">
             </div>
             <div class="faq-answer">
                 <p>Een huurpremie is meestal een financiële tegemoetkoming die wordt toegekend aan huurders met een laag inkomen en een hoge huurlast, terwijl een subsidie een bredere term is die verschillende vormen van financiële steun kan omvatten.</p>
-                <img src="path_to_het_juridisch_loket_logo" alt="Het Juridisch Loket Logo">
+                
             </div>
         </div>
         <div class="faq-item">
             <div class="faq-question">
                 <h3>2. Hoe wordt de hoogte van een huurpremie of subsidie bepaald?</h3>
+                 <img src="images/image6.png" alt="Vlaanderen Logo">
             </div>
             <div class="faq-answer">
                 <p>De hoogte wordt vaak bepaald op basis van verschillende factoren, waaronder het inkomen van de huurder, de huurprijs van de woning en eventuele specifieke behoeften of omstandigheden.</p>
-                <img src="path_to_vlaanderen_logo" alt="Vlaanderen Logo">
+               
             </div>
         </div>
         <div class="faq-item">
             <div class="faq-question">
                 <h3>3. Wat moet ik doen als mijn inkomen verandert tijdens de looptijd van de premie of subsidie?</h3>
+                <img src="images/image6.png" alt="Vlaanderen Logo">
             </div>
             <div class="faq-answer">
                 <p>Het is belangrijk om eventuele wijzigingen in je inkomen tijdig door te geven aan de relevante instantie om ervoor te zorgen dat je recht blijft hebben op de premie of subsidie.</p>
-                <img src="path_to_vlaanderen_logo" alt="Vlaanderen Logo">
+                
             </div>
         </div>
         <div class="faq-item">
             <div class="faq-question">
                 <h3>4. Zijn er specifieke huurpremieprogramma's voor mensen met een handicap en zo ja, welke criteria worden gebruikt om de mate van ondersteuning te bepalen?</h3>
+                <img src="images/3.png" alt="KU Leuven Logo">
             </div>
             <div class="faq-answer">
                 <p>Sommige regio's bieden specifieke huurpremieprogramma's voor mensen met een handicap. Naast financiële gegevens kunnen criteria voor de mate van ondersteuning factoren omvatten zoals de aard en ernst van de handicap, de noodzaak van aangepaste huisvesting en de beschikbaarheid van ondersteunende diensten.</p>
-                <img src="path_to_ku_leuven_logo" alt="KU Leuven Logo">
+                
             </div>
         </div>
     </section>
